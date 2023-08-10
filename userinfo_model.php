1151928704cd3b731687c7228231a99fbeb1c52e
tanvir103 <tanvirh103@gmail.com>
2023-08-11 04:03:15 +0600

    .

--- userinfo_model.php
+++ userinfo_model.php
@@ -4,6 +4,7 @@ function userinfo($username,$password){
     $conn=dbConnection();
     $sql="SELECT * FROM userinfo WHERE username='$username' AND password='$password'";
     $result=mysqli_query($conn,$sql);
-    return $result;
+    $row = mysqli_fetch_assoc($result);
+    return $row;
 }
 ?>
