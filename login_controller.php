ed9708ff4612ee5685332fd4fe14127090e66b69
tanvir103 <tanvirh103@gmail.com>
2023-08-11 03:46:48 +0600

    .

--- login_controller.php
+++ login_controller.php
@@ -1,6 +1,17 @@
 <?php
-require_once('datebase.php');
-
+require_once('userinfo_model.php');
+if(isset($_POST['submit'])){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $row=userinfo($username,$password);
+    if($row['role']=="User"){
+        header('location:user_dashboard.html');
+    }else if($row['role']=="Admin"){
+        header('location:admin_dashboard.html');
+    }else{
+        header('location:wrong.html');
+    }
+}
 
 
 ?>
--- userinfo_model.php
+++ userinfo_model.php
@@ -1,4 +1,9 @@
 <?php
 require_once('database.php');
-
+function userinfo($username,$password){
+    $conn=dbConnection();
+    $sql="SELECT * FROM userinfo WHERE username='$username' AND password='$password'";
+    $result=mysqli_query($conn,$sql);
+    return $result;
+}
 ?>
